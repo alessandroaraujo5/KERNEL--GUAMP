b7421d088b73d984eddb61e94bf94c1c6ffe9bff
Mcdaleste <159117678+alessandroaraujo5@users.noreply.github.com>
2025-12-16 09:23:24 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ function KERNEL_COMPILE() {
 	git restore drivers/Makefile drivers/Kconfig
 
 	# Setup for KernelSU
-	curl -LSs "https://raw.githubusercontent.com/kylieeXD/SukiSU-Ultra/main/kernel/setup.sh" | bash -s tmp-builtin
+	curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
 
 	# Download clang if not present
 	if [[ ! -d clang ]]; then mkdir -p clang
